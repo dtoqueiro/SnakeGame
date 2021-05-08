35449d85a3eaed7c4f97d6563a0a2894ac9ad825
Daniel Lins Toqueiro <dtoqueiro@gmail.com>
2021-05-08 10:59:50 -0300

    Adiciona movimento da Cobrinha com o Teclado

--- script.js
+++ script.js
@@ -22,6 +22,23 @@ function criarCobrinha() {
   }
 }
 
+document.addEventListener("keydown", update);
+
+function update(event) {
+  /* KeyCodes:
+    "left":   37,
+    "right":  39,
+    "up":     38,
+    "down":   40,
+  */
+
+  // A Direção não pode a oposta da direção atual;
+  if (event.keyCode === 37 && direction !== "right") direction = "left";
+  if (event.keyCode === 38 && direction !== "down") direction = "up";
+  if (event.keyCode === 39 && direction !== "left") direction = "right";
+  if (event.key === "ArrowDown" && direction !== "up") direction = "down";
+}
+
 function iniciarJogo() {
   criarBG();
   criarCobrinha();

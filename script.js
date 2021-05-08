00cb330798bc16af4ef1ffdef21b2c1405fee566
Daniel Lins Toqueiro <dtoqueiro@gmail.com>
2021-05-08 18:27:28 -0300

    Adiciona Colisão com o corpo

--- script.js
+++ script.js
@@ -58,6 +58,13 @@ function iniciarJogo() {
   if (snake[0].y > 15 * box) snake[0].y = 0 * box;
   if (snake[0].y < 0 * box) snake[0].y = 16 * box;
 
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
+      clearInterval(jogo);
+      alert("Game Over :( ");
+    }
+  }
+
   criarBG();
   criarCobrinha();
   desenhaComida();
